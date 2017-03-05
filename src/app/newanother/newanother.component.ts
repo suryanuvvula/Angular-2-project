e158695325b8a2cd882e87f8c43ec0ecd846c25a
suryateja <surya.ui@gmail.com>
2017-03-05 15:09:41 -0500

    New Another component commit

---
+++ src/app/newanother/newanother.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newanother',
+  templateUrl: './newanother.component.html',
+  styleUrls: ['./newanother.component.css']
+})
+export class NewanotherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
